53d21fed32a9dc4c3dd3af7e7bfe81a3a061db0f
Ferret <gfrikpost@gmail.com>
2013-11-06 19:18:22 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Volhovec::Application.config.secret_key_base = '94c3ac69b45da9e7379a890b9c5fa8682bf237d0e884f5648b0ca2b59bb0e5d4defa813fde87e02464350ddea7647e4ae394ca58812f7104bcaa6759d51a635b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Volhovec::Application.config.session_store :cookie_store, key: '_volhovec_session'
