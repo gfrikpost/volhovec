79999d325330b5023ae9ed65c651a8fddf5c73c7
Aleksey Horkov <a.horkov@leadsleader.ru>
2014-05-05 03:05:34 +0400

    Try to include all css and js

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(:default, Rails.env)
 
 module Volhovec
   class Application < Rails::Application
-    
+
     config.to_prepare do
       # Load application's model / class decorators
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
@@ -32,5 +32,7 @@ module Volhovec
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
      config.i18n.default_locale = :ru
+     config.assets.precompile << '*.js'
+     config.assets.precompile << '*.css'
   end
 end
