b56ac9ca10ceac018647cf13bf18db19d938a573
Ferret <gfrikpost@gmail.com>
2015-08-06 20:11:26 +0000

    added slider

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150731210651) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "metering_orders", force: true do |t|
     t.string   "name"
     t.string   "phone"
